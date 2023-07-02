e16fc0e8d9429a6c7f96ca89128e314a3eb71dc0
Gabriela Menezes <gabimenezess10@gmail.com>
2023-07-02 08:54:07 -0300

    feat: add dumb component Banner

---
+++ src/components/Banner/index.js
@@ -0,0 +1,9 @@
+import './Banner.css'
+const Banner = () => {
+    return (
+        <header className='banner'>
+            <img src='./imagens/banner.png' alt="Banner principal da página do Organo" />
+        </header>
+    )
+}
+export default Banner;
