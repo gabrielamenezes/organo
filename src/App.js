9e5d4d6e68e750ceeb9f9c6786b182c51770510f
Gabriela Menezes <gabimenezess10@gmail.com>
2023-07-02 09:02:33 -0300

    feat: add dinamic team selection

--- src/App.js
+++ src/App.js
@@ -1,10 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import Banner from './components/Banner'
+import Time from './components/Time'
+import Formulario from './components/Formulario'
 
 function App() {
+
+  const times = [
+    {
+      nome: 'Programação',
+      corPrimaria: '#57C278',
+      corSecundaria: '#D9F7E9'
+    },
+    {
+      nome:'Front-End',
+      corPrimaria: '#82CFFA',
+      corSecundaria: '#E8F8FF'
+    },
+    {
+      nome:'Data Science',
+      corPrimaria: '#A6D157',
+      corSecundaria: '#F0F8E2'
+    },
+    {
+      nome:'Devops',
+      corPrimaria: '#E06B69',
+      corSecundaria: '#FDE7E8'
+    },
+    {
+      nome:'UX e Design',
+      corPrimaria: '#DB6EBF',
+      corSecundaria: '#FAE9F5'
+    },
+    {
+      nome:'Mobile',
+      corPrimaria: '#FFBA05',
+      corSecundaria: '#FFF5D9'
+    },
+    {
+      nome:'Inovação e Gestão',
+      corPrimaria: '#FF8A29',
+      corSecundaria: '#FFEEDF'
+    },
+  ]
+
+  const [colaboradores, setColaboradores] = useState([])
+  const aoNovoColaboradorAdicionado = (colaborador) => {
+      setColaboradores([...colaboradores, colaborador]) // cria um novo array e coloca todos os colaboradores anteriores e adiciona o novo no final
+      console.log(colaboradores)
+  }
   return (
     <div className="App">
-      
+      <Banner />
+      <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
+      {times.map(time => <Time nome={time.nome}/>)}
     </div>
   );
 }
