89e966debc9ca45e4532cd66eaec1687c40dedd4
Gabriela Menezes <gabimenezess10@gmail.com>
2023-07-02 20:34:38 -0300

    feat: add colaborador component and its style

---
+++ src/components/Colaborador/index.js
@@ -0,0 +1,17 @@
+import './Colaborador.css'
+const Colaborador = (props) => {
+    return (
+        <div className='colaborador'>
+            <div className='cabecalho'>
+                <img src='https://github.com/gabrielamenezes.png' alt='Gabriela Menezes - Desenvolvedora'/>
+            </div>
+
+            <div className='rodape'>
+                <h4>Gabriela Menezes</h4>
+                <h5>Desenvolvedora</h5>
+            </div>
+        </div>
+    )
+}
+
+export default Colaborador
