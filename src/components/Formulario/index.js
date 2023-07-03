eb635cc77df0e82996d947192f0a8a4c97fb3fcd
Gabriela Menezes <gabimenezess10@gmail.com>
2023-07-03 18:56:17 -0300

    feat: reset input values when submit form

--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -18,6 +18,10 @@ const Formulario = (props) => {
             imagem, 
             time
         })
+        setNome('')
+        setCargo('')
+        setImagem('')
+        setTime('')
     }
 
     return (
