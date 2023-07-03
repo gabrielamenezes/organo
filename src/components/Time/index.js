4c7d0e863262d2af9b81ecb8f5415a7137aa199d
Gabriela Menezes <gabimenezess10@gmail.com>
2023-07-03 18:57:28 -0300

    feat: add condicional render - only show team component if they have a person inside it

--- src/components/Time/index.js
+++ src/components/Time/index.js
@@ -3,6 +3,8 @@ import Colaborador from '../Colaborador/'
 // não tem nenhum comportamento, só recebe props e exibe na tela - DUMB COMPONENT (componente visual)
 const Time = (props) => {
     return (
+        //renderização condicional
+        props.colaboradores.length > 0 &&
         <section className='time' style={{backgroundColor: props.background}}>
             <h3 style={{borderBottom: '4px solid' + props.corCard}}>{props.nome}</h3>
             <div className='colaboradores'>
